78502d9884bfee193d2a4d4d60d7165ebce6ecc5
Henrik Larsson <felvaggad@hotmail.com>
2018-06-20 10:42:25 +0200

    super mega solid

---
+++ src/ShoppingCart/Shop/Discounts/Discount.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TerriBad.ShoppingCart
+{
+    public class Discount : IDiscount
+    {
+        public decimal CalculateLeveledDiscout(IList<ICartItem> items)
+        {
+            if (items.Count >= 5 && items.Count < 10) return .10m;
+            else if (items.Count >= 10 && items.Count < 15) return .15m;
+            else return 0m;
+        }
+    }
+}
---
+++ src/ShoppingCart/Shop/Factories/ShopFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace TerriBad.ShoppingCart
+{
+    public static class ShopFactory
+    {        
+        public static IShoppingCart CreateShoppingCart()
+        {
+            return new ShoppingCart();
+        }
+
+        public static ICartItem CreateItem()
+        {
+            return new CartItem();
+        }
+
+        public static IDiscount CreateDiscount()
+        {
+            return new Discount();
+        }
+    }
+}
