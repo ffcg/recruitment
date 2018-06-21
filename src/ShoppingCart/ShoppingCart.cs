d24d09b43733d3802304cd810439e3c4d619d052
Obskyrion <40440453+Obskyrion@users.noreply.github.com>
2018-06-21 09:07:45 +0200

    Update ShoppingCart.cs

--- src/ShoppingCart/ShoppingCart.cs
+++ src/ShoppingCart/ShoppingCart.cs
@@ -11,20 +11,29 @@ namespace Bad.ShoppingCart
             _items = new List<CartItem>();
         }
 
-        public decimal GetDiscountPercentage()
+        public decimal GetDiscountPercentage(Discount discount = null) //Flera olika modeller av rabatter möjliga
         {
-            decimal amount = 0;
-
-            if (_items.Count >= 5 && _items.Count < 10)
-            {
-                amount = 10;
+            /*
+                Har inte så bra koll på programmeringssyntax längre (se ovan parameter som säkert är fel)
+                Ber också om ursäkt för tankevurpor och metoder som inte finns :)
+            */
+           
+            /* Standardvärden först och lite felkoll */
+            int interval = 5;
+            int maxDiscount = 50 // = 50 vi vill ju inte ge 120% rabatt (se senare) om kunden köper väldigt många varor.
+            int firstDiscountStep = 10
+            if(discount != null) {
+                int interval = discount.getInterval(); //Från databasmodellen
+                int maxDiscount = discount.getMaxDiscount(); 
+                int firstDiscountStep = discount.getFirstDiscountStep();
             }
-            else if (_items.Count >= 10 && _items.Count < 15)
-            {
-                amount = 15;
-            }
+            
+            /* Tänkte mig följande scenario med en if-sats */
+        
+            int step = floor(_items.Count/interval)*interval;
+            int amount = step + sign(step)*(firstDiscountStep-interval);
+            return amount > maxDiscount ? maxDiscount : amount;
 
-            return amount;
         }
 
         public void Add(CartItem cartItem)
